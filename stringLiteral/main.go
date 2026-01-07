568a73116d834bcf66536b21ce33d81063777f4e
certainlyMohneeesh <certainlymohneesh@gmail.com>
2026-01-07 22:54:40 +0530

    String Literal

---
+++ stringLiteral/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	fmt.Println(`
+	This is a multi line string literal.
+	It preserves whitespace and line breaks.
+	You can include "quotes" without escaping.
+	`)
+}
