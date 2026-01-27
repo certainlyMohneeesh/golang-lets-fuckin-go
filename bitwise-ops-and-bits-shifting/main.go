e3c1da1342ae69598a1348c42c8eed918fc2da4f
certainlyMohneeesh <certainlymohneesh@gmail.com>
2026-01-28 00:18:20 +0530

    Refactor bitwise operations to use constants for better readability

--- bitwise-ops-and-bits-shifting/main.go
+++ bitwise-ops-and-bits-shifting/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+const (
+	_ = iota
+	a
+	b
+	c
+	d
+	e
+	f
+)
+
 func main() {
 	fmt.Printf("%d \t %b\n", 1, 1)
-	fmt.Printf("%d \t %b\n", 1<<1, 1<<1)
-	fmt.Printf("%d \t %b\n", 1<<2, 1<<2)
-	fmt.Printf("%d \t %b\n", 1<<3, 1<<3)
-	fmt.Printf("%d \t %b\n", 1<<4, 1<<4)
-	fmt.Printf("%d \t %b\n", 1<<5, 1<<5)
-	fmt.Printf("%d \t %b\n", 1<<6, 1<<6)
+	fmt.Printf("%d \t %b\n", 1<<a, 1<<a)
+	fmt.Printf("%d \t %b\n", 1<<b, 1<<b)
+	fmt.Printf("%d \t %b\n", 1<<c, 1<<c)
+	fmt.Printf("%d \t %b\n", 1<<d, 1<<d)
+	fmt.Printf("%d \t %b\n", 1<<e, 1<<e)
+	fmt.Printf("%d \t %b\n", 1<<f, 1<<f)
 }
