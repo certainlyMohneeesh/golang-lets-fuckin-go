efe31b0f58921e44b7956619acb6ee7fc39d0dfa
certainlyMohneeesh <certainlymohneesh@gmail.com>
2026-01-29 16:17:04 +0530

    All over puppy

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lets-fuckin-go
 
 go 1.25.6
+
+require github.com/certainlyMohneeesh/golang-lets-fuckin-go/puppy v0.0.0-20260129103351-aa1c79cdb898 // indirect
--- just-module/main.go
+++ just-module/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/certainlyMohneeesh/golang-lets-fuckin-go/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	fmt.Printf("%v\n%v\n", s1, s2)
+}
