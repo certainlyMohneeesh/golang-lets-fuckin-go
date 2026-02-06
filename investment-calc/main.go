1ff0c43b3590da7d94c704d6355946bf4a0df84c
certainlyMohneeesh <certainlymohneesh@gmail.com>
2026-02-06 17:20:33 +0530

    Use function for Printing stuff

--- investment-calc/main.go
+++ investment-calc/main.go
@@ -11,12 +11,16 @@ func main() {
 	years := 10.0
 	expectedReturnRate := 5.5
 
-	fmt.Print("Enter Your Investment Amount: ")
+	outputText("Enter Your Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Printf("The future value will be %v\n", futureValue)
-	fmt.Printf("The future real value will be %v\n", futureRealValue)
+	fmt.Printf("Future Value: %v\n", futureValue)
+	fmt.Printf("Future Value (adjusted for Inflation):  %v\n", futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
